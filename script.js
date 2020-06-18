20ce60fede2fece32c5bc263a7a0e4b839c57da4
Yura Kim <keyura@keti.re.kr>
2020-06-18 13:13:14 +0900

    dropbox test

--- script.js
+++ script.js
@@ -20,6 +20,26 @@ function move(){
 	}
 }
 
+toggle between hiding and showing the dropdown content */
+function input_dropbox() {
+  document.getElementById("inputDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
 
 // define the callAPI function that takes a first name and last name as parameters
 var callAPI = (firstName,lastName)=>{
