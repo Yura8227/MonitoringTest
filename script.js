383268ed24efdabc88691e399fc51efb2f7ac5ad
Yura Kim <keyura@keti.re.kr>
2020-07-01 17:42:02 +0900

    Lambda test

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 
 
+// define the callAPI function that takes a first name and last name as parameters
+function callAPI(){
+	// instantiate a headers object
+	var myHeaders = new Headers();
+	// add content type header to object
+	myHeaders.append("Content-Type", "application/json");
+	// using built in JSON utility package turn object to string and store in a variable
+	var raw = JSON.stringify(" ");
+	// create a JSON object with parameters for API call and store in a variable
+	var requestOptions = {
+		method: 'POST',
+		headers: myHeaders,
+		body: raw,
+		redirect: 'follow'
+	};
+	// make API call with parameters and use promises to get response
+	fetch("https://lpb2pszwab.execute-api.ap-northeast-2.amazonaws.com/dev", requestOptions)
+	.then(response => response.text())
+	.then(result => alert(JSON.parse(result).body))
+	.catch(error => console.log('error', error));			
+}
 
 var i=0;
 function move(){
@@ -7,8 +28,9 @@ function move(){
 		i =1;
 		var elem = document.getElementById("myBar");
 		var width =1;
+		callAPI();			
 		var id = setInterval(frame,10);
-		function frame(){
+		function frame(){			
 			if(width >=100)	{
 				clearInterval(id);
 				i=0;
@@ -29,9 +51,9 @@ function input_dropbox() {
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
+    var idx;
+    for (idx = 0; idx < dropdowns.length; idx++) {
+      var openDropdown = dropdowns[idx];
       if (openDropdown.classList.contains('show')) {
         openDropdown.classList.remove('show');
       }
@@ -41,24 +63,4 @@ window.onclick = function(event) {
 
 
 
-// define the callAPI function that takes a first name and last name as parameters
-var callAPI = (firstName,lastName)=>{
-	// instantiate a headers object
-	var myHeaders = new Headers();
-	// add content type header to object
-	myHeaders.append("Content-Type", "application/json");
-	// using built in JSON utility package turn object to string and store in a variable
-	var raw = JSON.stringify({"firstName":firstName,"lastName":lastName});
-	// create a JSON object with parameters for API call and store in a variable
-	var requestOptions = {
-		method: 'POST',
-		headers: myHeaders,
-		body: raw,
-		redirect: 'follow'
-	};
-	// make API call with parameters and use promises to get response
-	fetch("https://lpb2pszwab.execute-api.ap-northeast-2.amazonaws.com/dev", requestOptions)
-	.then(response => response.text())
-	.then(result => alert(JSON.parse(result).body))
-	.catch(error => console.log('error', error));			
-}
+
