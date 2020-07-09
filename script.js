c680282564ba04bf50717c01188093c901a3145b
Yura Kim <keyura@keti.re.kr>
2020-07-09 10:32:18 +0900

    change btn to check box applied div testing run btn for API Gateway

--- script.js
+++ script.js
@@ -22,18 +22,49 @@ function callWebTestAPI(workflow){
 	.catch(error => console.log('error', error));			
 }
 
-var i=0;
+function CallWFAPIGateWay(workflow){
+	// instantiate a headers object
+	var myHeaders = new Headers();
+	// add content type header to object
+	myHeaders.append("Content-Type", "application/json");
+	// using built in JSON utility package turn object to string and store in a variable
+	var raw = JSON.stringify({"firstName":workflow });
+	// create a JSON object with parameters for API call and store in a variable
+	var requestOptions = {
+		method: 'POST',
+		headers: myHeaders,
+		body: raw,
+		redirect: 'follow'
+	};
+	// make API call with parameters and use promises to get response
+	fetch("https://lpb2pszwab.execute-api.ap-northeast-2.amazonaws.com/dev", requestOptions)
+	.then(response => response.text())
+	.then(result => alert(JSON.parse(result).body))
+	.catch(error => console.log('error', error));			
+}
+
+var progress_value=0;
 function move(){
-	if(i==0){
-		i =1;
+	if(progress_value==0){
+		progress_value =1;
+		var cb = document.querySelectorAll('input:checked');
+		
+		if( cb.length ==0 )
+			return;
+		for(idx = 0; idx< cb.length; idx++)
+		{
+			CallWFAPIGateWay(cb[idx].value);			
+		}
+
+		// progress bar control
 		var elem = document.getElementById("myBar");
 		var width =1;
-		callWebTestAPI();			
+		//callWebTestAPI();			
 		var id = setInterval(frame,10);
 		function frame(){			
 			if(width >=100)	{
 				clearInterval(id);
-				i=0;
+				progress_value=0;
 			}else{
 				width++;
 				elem.style.width = width + "%";
@@ -60,6 +91,3 @@ window.onclick = function(event) {
     }
   }
 }
-
-
-
